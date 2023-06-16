a74f3fe16577f4ecf31e80d275537184ee04746d
BasantMasnour <basantmansour475@gmail.com>
2023-06-15 19:56:56 -0700

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#includ <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : A C prodram prints with printf function
+ *
+ * Return: Always 0 (Success)
+*/
+
+intt main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
