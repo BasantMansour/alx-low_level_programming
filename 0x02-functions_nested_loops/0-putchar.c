3912ea1c6bbe1b94540e3fae0b8058f5de792aa7
BasantMasnour <basantmansour475@gmail.com>
2023-06-20 13:54:44 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description:  positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
