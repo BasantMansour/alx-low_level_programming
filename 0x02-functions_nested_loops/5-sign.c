9be94c6f327912da355a83f8c51790e6d6eb30d2
BasantMasnour <basantmansour475@gmail.com>
2023-06-20 18:46:26 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - this function to print sign to the input
+ * if it + , - , 0
+ *
+ * @n: type input of function
+ *
+ * Return: 1 if +, 0 if 0, -1 if -
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	else
+	{
+		printf("-");
+		return (-1);
+	}
+}
