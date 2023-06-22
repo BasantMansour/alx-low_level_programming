7ef84badf4556fbf3685d2bc9036fa6bf267cad7
BasantMasnour <basantmansour475@gmail.com>
2023-06-21 17:25:44 -0700

    fix bug

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
-		for (mlt = 0; mlt <= 9; mlt++)
+		for (mlt = 1; mlt <= 9; mlt++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -25,8 +25,8 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar((prod / 10 ) + 48);
-				_putchar((prod % 10 ) + 48);
+				_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
 			}
 		}
 		_putchar('\n');
