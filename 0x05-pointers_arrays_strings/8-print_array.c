a97e8985ab62da7ad470e54e6bdc2940b5034e65
BasantMasnour <basantmansour475@gmail.com>
2023-06-26 19:21:30 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - print n of an array of integer
+ *
+ * @n: paramters input
+ * @a: string input
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n")
+}
