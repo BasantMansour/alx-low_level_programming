70d0199002b9af42e8edbe912383468c0dc3a940
BasantMasnour <basantmansour475@gmail.com>
2023-07-05 14:16:50 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print string followed by new line
+ * @s: in put of string
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+	}
+	else
+	{
+		putchar(*s);
+		puts_recursion(s + 1);
+	}
+}
