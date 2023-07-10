1666c65cd61da74326f808fb41ac03b37525fba6
BasantMasnour <basantmansour475@gmail.com>
2023-07-10 08:16:34 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main print sum of 2 number
+ * @argc: number of the command line
+ * @argv: pointer to an array of command line
+ * Return: 0-success, non-zero-fail
+*/
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
