6dae0da79ba2b2743641cd0377059a1bc34031b6
BasantMasnour <basantmansour475@gmail.com>
2023-07-10 08:27:59 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 
 /**
- * main print sum of 2 number
- * @argc: number of the command line
- * @argv: pointer to an array of command line
- * Return: 0-success, non-zero-fail
+ * main print sum of 2 number.
+ * @argc:number of the command line.
+ * @argv:pointer to an array of command line.
+ * Return:0-success, non-zero-fail.
 */
 
 int main(int argc, char *argv[])
