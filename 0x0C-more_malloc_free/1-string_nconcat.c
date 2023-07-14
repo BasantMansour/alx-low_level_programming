5abf21986808de9cd132caee31aecabafd15776c
BasantMasnour <basantmansour475@gmail.com>
2023-07-13 18:39:45 -0700

    string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,12 +5,12 @@
  * @s1: it is pointer.
  * @s2: it is pointer.
  * @n: it is number of bytes.
- * Return: m or NULL.
+ * Return: c or NULL.
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *m;
+	char *c;
 	unsigned int e, a, S1, S2;
 
 	if (s1 == NULL)
@@ -21,19 +21,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	for (S2 = 0; s2[S2] != '\0'; S2++)
 		;
-	m = malloc(S1 + n + 1);
-	if (m == NULL)
+	c = malloc(S1 + n + 1);
+	if (c == NULL)
 	{
 		return (NULL);
 	}
 	for (e = 0; s1[e] != '\0'; e++)
-		m[e] = s1[e];
+		c[e] = s1[e];
 	for (a = 0; a < n; a++)
 	{
-		m[e] = s2[a];
+		c[e] = s2[a];
 		e++;
 	}
 
-	m[e] = '\0';
-	return (m);
+	c[e] = '\0';
+	return (c);
 }
