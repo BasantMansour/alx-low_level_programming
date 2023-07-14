438cd7431de51b0fbe388320e75f317b168c501f
BasantMasnour <basantmansour475@gmail.com>
2023-07-13 19:03:03 -0700

    main

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,17 +3,17 @@
 /**
  * *malloc_checked - function that allocates memory using malloc.
  * @b: it is a b
- * Return: s or NULL.
+ * Return: r or NULL.
 */
 
 void *malloc_checked(unsigned int b)
 {
-	int *s = malloc(b);
+	int *r = malloc(b);
 
-	if (s == 0)
+	if (r == 0)
 	{
 		exit(98);
 	}
 
-	return (s);
+	return (r);
 }
