a564e5d255c0d53ba2de20a0ae7da769db781753
BasantMansour <basantmansour475@gmail.com>
2023-08-08 14:52:04 -0700

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 	{
 		for (y = 0; y < height; y++)
 		{
-			tab[y] = malloc(sizeof(**tab) * which);
+			tab[y] = malloc(sizeof(**tab) * width);
 			if (tab[y] == 0)
 			{
 
