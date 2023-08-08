f5e366e82d5f7cf6f5bf57baa965ba7e748e08d8
BasantMansour <basantmansour475@gmail.com>
2023-08-08 16:03:07 -0700

    program

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - this program that prints all arguments it receives
+ * @argc: int
+ * @argv: list
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	int j = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[j]);
+		j++
+	}
+
+	return (0);
+}
