860a902c9f5f7560aec5e6c4bdcb0c1acb833c4e
BasantMansour <basantmansour475@gmail.com>
2023-08-14 17:21:23 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - this is a dog struct
+ * @name: this is dog name
+ * @age: this is dog age
+ * @owner: this is owner name
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
