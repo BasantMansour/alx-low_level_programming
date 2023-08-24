856705acb8701173504ee69cf047b58637bda963
BasantMansour <basantmansour475@gmail.com>
2023-08-24 15:05:05 -0700

    add node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,18 +14,18 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node = malloc(sizeof(list_t));
 	list_t *node = *head;
 
-        if (!head || !new_node)
-                return (NULL);
-        if (str)
-        {
-                new_node->str = strdup(str);
-                if (!new_head->str)
-                {
-                        free(new_node);
-                        return (NULL);
-                }
-                new_node->len = _strlen(new_node->str);
-        }
+	if (!head || !new_node)
+		return (NULL);
+	if (str)
+	{
+		new_node->str = strdup(str);
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->len = _strlen(new_node->str);
+	}
 	if (node)
 	{
 		while (node->next)
