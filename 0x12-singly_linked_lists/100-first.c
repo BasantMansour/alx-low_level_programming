ba6a0374acea95c31b818fee07b8fb59ca765c7a
BasantMansour <basantmansour475@gmail.com>
2023-08-24 15:35:30 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - this executes before main()
+ *
+ *
+ * Return: void
+*/
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n";
+			"I bore my house upon my back!\n");
+}
