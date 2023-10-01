005e0e2d1e85ff4aaf289c068b3adb8ad205ac7a
BasantMansour <basantmansour475@gmail.com>
2023-09-30 17:48:40 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - this print square using the character
+ *
+ * @size: this is a size of square
+ *
+ * Return: 0 always
+*/
+
+void print_square(int size)
+{
+	int row, colu;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (colu = 1; colu <= size; colu++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
