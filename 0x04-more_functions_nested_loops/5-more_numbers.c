e93d6e2a13f3792ee5fc30bd292d84f08210a866
BasantMansour <basantmansour475@gmail.com>
2023-09-30 17:26:26 -0700

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,28 @@
 #include "main.h"
 #include <stdio.h>
 
-/***/
+/**
+ * more_numbers - this print 0 - 14 ten times
+ *
+ * Return: always 0
+*/
 
 void more_numbers(void)
 {
+	int nun, row, coun;
 
+	for (row = 1; row <= 10; row++)
+	{
+		for (coun = 0; coun <= 14; coun++)
+		{
+			nun = coun;
+			if (coun > 9)
+			{
+				_putchar(1 + 48);
+				nun = coun % 10;
+			}
+			_putchar(nun + 48);
+		}
+		_putchar('\n');
+	}
+}
