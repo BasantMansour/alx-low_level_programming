c3d6f7648e0b4f4cba123c67b811ea1486166bfb
BasantMansour <basantmansour475@gmail.com>
2023-09-30 17:05:06 -0700

    edit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -12,8 +12,8 @@ void print_numbers(void)
 	int number = 0;
 
 	do {
-		putchar(number + 48);
+		_putchar(number + 48);
 		number++;
 	} while (number >= 0 && number <= 9);
-	putchar('\n')
+	_putchar('\n')
 }
